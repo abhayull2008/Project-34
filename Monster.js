8fc334aa424df6bf558951e23d17b4cb271b3a07
abhayull2008 <75789414+abhayull2008@users.noreply.github.com>
2021-04-02 08:41:47 +0530

    Add files via upload

---
+++ Monster.js
@@ -0,0 +1,26 @@
+class Monster {
+    constructor(x, y, r) {
+        var options = {
+            density: 1,
+            frictionAir: 1,
+        };
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.image = loadImage("images/Monster-01.png");
+        this.body = Bodies.circle(this.x, this.y, (this.r) / 2, options)
+        World.add(world, this.body);
+
+    }
+    display() {
+        push()
+        var heroPos = this.body.position
+        translate(heroPos.x, heroPos.y - 100);
+        rectMode(CENTER)
+        fill(255, 0, 255)
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.r + 150, this.r)
+        pop()
+
+    }
+}
